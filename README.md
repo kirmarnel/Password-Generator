e11266905c6542f545d3034a8f71d598586d6dbb
Kirsten Nelson <kirmarnel@gmail.com>
2021-02-21 13:38:21 -0800

    Add comments to code and additional information to README

--- README.md
+++ README.md
@@ -8,4 +8,14 @@ The purpose of this project was to create a password-generator where users could
 * Whether or not to include upper case letters
 * Whether or not to include lower case letters
 
-![Screenshot] (assets/image/Screenshot.png) 
+To start I was given some starter code which included all the html, css and some javascript. To the html I added an input box for the user to enter their desired number of characters into, along with check boxes for the other criteria listed above. For the javascript portion I added random generator functions, some if statements to correspond with my my above criteria and some for loops to select the characters needed from my functions. 
+---
+##Demo
+![Screenshot1](assets/images/Demo1.png) 
+![Screenshot2](assets/images/Demo2.png)
+---
+##Challenges and Resources
+One of the things I found most challenging about this project was creating functions that randomly generated upper and lower case letters, numbers and symbols. To make this happen I used the following method found in the following video:
+
+![Code](assets/images/ReferenceCode.png) 
+Reference Video: https://youtu.be/duNmhKgtcsI 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,16 @@
+//Function for random number
 function randomNumber() {
   return String.fromCharCode (Math.floor(Math.random() * 10) + 48);
-
 }
-
+//Function for Random upper case letter
 function randomUpper() {
   return String.fromCharCode (Math.floor(Math.random() * 26) + 65);
 }
-
+//Function for random lower case letter
 function randomLower() {
   return String.fromCharCode (Math.floor(Math.random() * 26) + 97);
 }
-
+//Function for random symbol
 function randomSymbol() {
   var symbols = "!@#$%^&*~<>.,"
   return symbols [Math.floor (Math.random()* symbols.length)];
@@ -31,16 +31,19 @@ function generatePassword() {
   var numberChar = document.getElementById("numberChar").value
 
   for (var i=0; i<numberChar; i++) {
-  
+    //If numbers are included
     if (document.querySelector("#numbers").checked) {
       passArray.push(randomNumber()) 
     } 
+    //If upper case are included
     if (document.querySelector("#upCase").checked) {
      passArray.push(randomUpper())  
     } 
+    //If lower case are included
     if (document.querySelector("#lowCase").checked) {    
       passArray.push(randomLower())
     } 
+    //If symbols are included
     if (document.querySelector("#symbols").checked) { 
       passArray.push(randomSymbol())
     }  
@@ -52,16 +55,5 @@ function generatePassword() {
       }
       return password
       }
-  
- 
-
-
-
-
-
-
-
-  
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
